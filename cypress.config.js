a313de25f824217d9185a4e62b2b5831cd767e38
trung <tranthanhtrung1983@yahoo.com>
2026-02-25 00:25:43 +0100

    cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -1,11 +1,16 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  allowCypressEnv: false,
-
   e2e: {
+    baseUrl: 'https://portfolio.com',  // Basis-URL für alle Tests
+    supportFile: 'cypress/support/e2e.js',
+    specPattern: 'cypress/e2e/**/*.cy.{js,ts}',
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      // Node Event Hooks für CI/CD, Plugins oder Tasks
     },
+  },
+  env: {
+    loginPath: '/login',
+    dashboardPath: '/dashboard'
   },
 });
